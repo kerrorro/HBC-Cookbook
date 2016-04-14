c18456064f2f7e9e52463fe721fc0040b75f3a06
kerrorro <caroline.chen1101@gmail.com>
2016-04-14 15:40:59 -0500

    Ran the crawler to obtain 2 recipes in the extractedRecipe.txt. All sql statements are working in "populateRecipeDB" but issues with processing the ingredient lines to obtain. May consider not processing and just saving quantity, measurement, and ingredient name in one string.

--- populateRecipeDB.py
+++ populateRecipeDB.py
@@ -1,7 +1,7 @@
 """
 Created by: Caroline Chen
 Date created: 4/11/16
-Date late modified: 4/13/16
+Date late modified: 4/14/16
 Description: Take a formatted txt file output by the Foodnetwork.com crawler and parser program
              and uses pymysql to populate a cookbook database.
 
@@ -18,22 +18,45 @@ Description: Take a formatted txt file output by the Foodnetwork.com crawler and
 
 import pymysql
 import re
-"""
+
 # Connect to database
-conn = pymysql.connect(host='127.0.0.1', port=3306, charset = 'utf8'
+conn = pymysql.connect(host='127.0.0.1', port=3306, charset = 'utf8',
                        user='root', passwd= '79461385258', db = 'Cookbook')
 cur = conn.cursor()
-"""
+
+# Placeholder "databases" to ensure duplicate entries are not inserted.
+# Ignore is for seen measurement candidates which have been determined not to be measurements
+ingredientDB = []
+measurementDB = []
+categoryDB = []
+subcategoryDB= []
+categorySubcatDB = []
+IGNORE = []
+
+def select(column, table, conditionCol = "", conditionVal = ""):
+    #sql = "SELECT ingredient_id FROM ingredient WHERE ingredient_name = %s" % (title)
+    sql = "SELECT %s FROM %s" % (column, table)
+    if (conditionCol != ""):
+        condition = " WHERE %s = %s" % (conditionCol, conditionVal)
+        sql = sql + condition
+    cur.execute(sql)
+    print("*** EXECUTED SQL STATEMENT ***")
+    print(sql)
+
+# Updates a column for specified field.
+def update(table, column, colVal, conditionCol, conditionVal):        
+    cur.execute("UPDATE %s SET %s = %s WHERE %s = '%s'" % (table, column, colVal, conditionCol, conditionVal))
+    cur.connection.commit()
+    print("*** EXECUTED SQL STATEMENT ***")
+    print("UPDATE %s SET %s = %s WHERE %s = '%s'" % (table, column, colVal, conditionCol, conditionVal))
+    
 
 # To insert multiple values into their corresponding columns, pass in as a string separated by commas
 def insert(table, columns, values):
-    #if (isinstance(value, str)):
-    #    print(("INSERT INTO %s (%s) VALUES ('%s')" % (table, columns, values)))
-    #elif(isinstance(value, int)):
-    #    print(("INSERT INTO %s (%s) VALUES (%s)" % (table, columns, values)))
-    print("INSERT INTO %s (%s) VALUES (%s)" % (table, columns, values))
-    cur.execute(("INSERT INTO %s (%s) VALUES (%s)" % (table, columns, values)))
+    cur.execute("INSERT INTO %s(%s) VALUES (%s)" % (table, columns, values))
     cur.connection.commit()
+    print("*** EXECUTED SQL STATEMENT ***")
+    print("INSERT INTO %s(%s) VALUES (%s)" % (table, columns, values))
 
 # Used to pass variables as strings into functions that change the database
 def strDB(text):
@@ -51,13 +74,18 @@ def processIngredient(ingredient):
 
     # Selects the next word as a potential candidate for being a measurement.
     # Asks user if the word should be included as a measurement
-    measurementCandidate = ingredient[:ingredient.find(" ")]
-    cur.execute ("SELECT measurement_type FROM measurement")
-    measurementDB = cur.fetchall()
-    
-    if (measurementCandidate not in measurementDB):
+    measurementCandidate = ingredient[:ingredient.find(" ")].lower()
+
+    # If the candidate has already been ignored before, there is no measurement in the string
+    if (measurementCandidate in IGNORE):
+        measurement = None
+    # If the candidate was already designated as a measurement:
+    elif (measurementCandidate in measurementDB):
+        measurement = measurementCandidate
+    # Otherwise, the candidate has never been seen before
+    else:
         validResponse = False
-        response = input(measurementCandidate + " was not found in the measurement table. \n"
+        response = input("'" + measurementCandidate + "' was not found in the measurement table. \n"
                               + "Do you want to add it to the table? (y/n): ")
         
         while (not validResponse):
@@ -68,15 +96,41 @@ def processIngredient(ingredient):
                 response = input("Please enter (y/n): ")
     
         if (response == "y"):
+            measurementDB.append(measurementCandidate)
             insert("measurement", "measurement_type", strDB(measurementCandidate))
             measurement = measurementCandidate     
         else:
+            IGNORE.append(measurementCandidate)
             measurement = None
-            
-    # If the candidate is already in the measurementDB
+    """    
+    # If the measurement candidate has never been seen before:
+    if ((measurementCandidate not in measurementDB) and (measurementCandidate not in IGNORE)):
+        validResponse = False
+        response = input("'" + measurementCandidate + "' was not found in the measurement table. \n"
+                              + "Do you want to add it to the table? (y/n): ")
+        
+        while (not validResponse):
+            if (response.lower() == "y" or response.lower() == "n"):
+                response = response.lower()
+                validResponse = True
+            else:
+                response = input("Please enter (y/n): ")
+    
+        if (response == "y"):
+            measurementDB.append(measurementCandidate)
+            insert("measurement", "measurement_type", strDB(measurementCandidate))
+            measurement = measurementCandidate     
+        else:
+            IGNORE.append(measurementCandidate)
+            measurement = None
+
+    # If the candidate has been seen but deemed not to be a measurement:
+    elif (measurementCandidate in IGNORE):
+        
+    # If the candidate is already in the measurementDB:
     else:
         measurement = measurementCandidate
-            
+    """       
     # Updates the ingredient string without the measurement
     if (measurement is not None):
         ingredient = ingredient[len(measurement) + 1:]
@@ -84,8 +138,6 @@ def processIngredient(ingredient):
     return [quantity, measurement, ingredient]
 
 
-def processCategories():
-    pass
 
 def is_number(s):
     try:
@@ -94,70 +146,88 @@ def is_number(s):
     except ValueError:
         return False
 
+def is_upper(s):
+    return s == s.upper()
+
 def main():
     
     file = open('extractedRecipes.txt', 'r')
     readingRecipe = True
     while (readingRecipe):
         line = file.readline().strip()
+        print ("-------------------------------------")
+        print("READING:", line)
         ingredients = []
-
-        # Title is not inserted until the other column information for the recipe table is obtained
         if (line == "TITLE"):
             title = file.readline().strip()
+            # Creates a field in the recipe table with null author and servings
+            insert("recipe", "recipe_name", strDB(title))
             # Grabs recipe_id for the added recipe
-            cur.execute("SELECT ingredient_id FROM ingredient WHERE ingredient_name = %s" % (title))
-            recipe_id = cur.fetchall()
+            select("recipe_id", "recipe", conditionCol = "recipe_name", conditionVal = strDB(title))
+            recipe_id = cur.fetchone()[0]
+            #print("*************RECIPE_id = ", recipe_id, type(recipe_id))
         elif (line == "INGREDIENTS"):
             ingredientLine = line
+            ingredientsPerRecipe = []
             while (ingredientLine != ""):
+                print()
                 ingredientLine = file.readline().strip()
+                if (ingredientLine == ""):
+                    continue
                 ingredientLineComp = processIngredient(ingredientLine)
                 quantity = ingredientLineComp[0]
                 measurement = ingredientLineComp[1]
                 ingredient = ingredientLineComp[2]
-                insert("ingredient", "ingredient_name", strDB(ingredient))
+                if (ingredient not in ingredientDB):
+                    insert("ingredient", "ingredient_name", strDB(ingredient))
+                    ingredientDB.append(ingredient)
                 # Grabs ingredient_id for the added ingredient
-                cur.execute("SELECT ingredient_id FROM ingredient WHERE ingredient_name = %s" % (ingredient))
-                ingredient_id = cur.fetchall()
-                # Updates junction table with recipe_id, ingredient_id, measurement_type, and quantity
-                if (quantity is not None):
-                    if (measurement is not None):
-                        # No null columns
-                        insert("recipeingredient", "recipe_id, ingredient_id, measurement_type, quantity",
-                                str(recipe_id) + ", " + str(ingredient_id) + ", " + strDB(measurement) + ", " + strDB(quantity))
+                select("ingredient_id", "ingredient", conditionCol = "ingredient_name", conditionVal = strDB(ingredient))
+                ingredient_id = cur.fetchone()[0]
+                #print("*************ingredient_id = ", ingredient_id, type(ingredient_id))
+                # Updates junction table with recipe_id, ingredient_id, measurement_type, and quantity if the ingredient isn't already listed for that recipe
+                if (ingredient not in ingredientsPerRecipe):
+                    ingredientsPerRecipe.append(ingredient)
+                    if (quantity is not None):
+                        if (measurement is not None):
+                            # No null columns
+                            insert("recipeingredient", "recipe_id, ingredient_id, measurement_type, quantity",
+                                    str(recipe_id) + ", " + str(ingredient_id) + ", " + strDB(measurement) + ", " + strDB(quantity))
+                        else:
+                            # Null measurement
+                            insert("recipeingredient", "recipe_id, ingredient_id, quantity",
+                                    str(recipe_id) + ", " + str(ingredient_id) + ", " + strDB(quantity))
                     else:
-                        # Null measurement
-                        insert("recipeingredient", "recipe_id, ingredient_id, quantity",
-                                str(recipe_id) + ", " + str(ingredient_id) + ", " + strDB(quantity))
-                else:
-                    if(measurement is not None):
-                        # Null quantity
-                        insert("recipeingredient", "recipe_id, ingredient_id, measurement_type",
-                                str(recipe_id) + ", " + str(ingredient_id) + ", " + strDB(measurement))
-                    else:
-                        # Null measurement and quantity
-                        insert("recipeingredient", "recipe_id, ingredient_id",
-                                str(recipe_id) + ", " + str(ingredient_id))    
+                        if(measurement is not None):
+                            # Null quantity
+                            insert("recipeingredient", "recipe_id, ingredient_id, measurement_type",
+                                    str(recipe_id) + ", " + str(ingredient_id) + ", " + strDB(measurement))
+                        else:
+                            # Null measurement and quantity
+                            insert("recipeingredient", "recipe_id, ingredient_id",
+                                    str(recipe_id) + ", " + str(ingredient_id))    
                     
         elif (line == "TIMES"):
             timeDict = {}
             # Counter is used to identify the double newline between the TIMES and SERVINGS sections
             consecNewLineCount = 0
-            while (consecLineCount != 2):
+            while (consecNewLineCount != 2):
                 timeLines = file.readline().strip()
                 if (timeLines == ""):
                     consecNewLineCount += 1
+                    continue
                 else:
                     consecNewLineCount = 0
-                if (not isNumber(timeLines) and timeLines != ""):
+                if (not is_number(timeLines)):
                     timeDict[timeLines] = file.readline().strip()
             for time_type, minutes in timeDict.items():
                 insert("time", "recipe_id, time_type, minutes",
                        str(recipe_id) + ", " + strDB(time_type) + ", " + str(minutes))
 
         elif (line == "SERVINGS"):
-            servings = file.readline().strip() ############################ NEEDS INSERT STATEMENT ##################################
+            # Updates the recipe table with the recipe's serving
+            servings = file.readline().strip()
+            update("recipe", "servings", strDB(servings), "recipe_id", str(recipe_id))            
             
         elif (line == "LEVEL"):
             level = file.readline().strip()
@@ -165,31 +235,61 @@ def main():
                        str(recipe_id) + ", " + strDB(level))
             
         elif (line == "AUTHOR"):
+            # Updates the recipe table with the recipe's author(s)
             author = file.readline().strip()
-            insert("recipe", "recipe_id, recipe_name, author",
-                       str(recipe_id) + ", " + strDB(recipe_name) + ", " + strDB(author))
+            update("recipe", "author_name", strDB(author), "recipe_id", str(recipe_id))
             
         elif (line == "DIRECTIONS"):
             directionLine = line
             directionList = []
             while (directionLine != ""):
-                directionList.append(file.readline().strip())
+                
+                directionLine = file.readline().strip()
+                if (directionLine == ""):
+                    break
+                directionList.append(directionLine)
             for i in range(len(directionList)):
+                print()
                 insert("directions", "recipe_id, step_number, directions",
                        str(recipe_id) + ", " + str(i+1) + ", " + strDB(directionList[i]))
                 
         elif (line == "CATEGORIES"):
-            categoryLine = line
-            categoryDict = {}
-                
+            consecNewLineCount = 0
+            while (consecNewLineCount != 2):
+                print()
+                categoryLine = file.readline().strip()
+                if (categoryLine == ""):
+                    consecNewLineCount += 1
+                    continue
+                else:
+                    consecNewLineCount = 0
+                if (is_upper(categoryLine)):
+                    if (categoryLine not in categoryDB):
+                        categoryDB.append(categoryLine)
+                        insert("category", "category_type", strDB(categoryLine))
+                    subcategoryList = []
+                    subcategoryLine = categoryLine
+                    while(subcategoryLine != ""):
+                        subcategoryLine = file.readline().strip()
+                        if (subcategoryLine == ""):
+                            consecNewLineCount += 1
+                            break
+                        subcategoryList.append(subcategoryLine)
+                    for subcategory in subcategoryList:
+                        if (subcategory not in subcategoryDB):
+                            subcategoryDB.append(subcategory)
+                            insert("subcategory", "subcategory_type", strDB(subcategory))
+                        categorySubcategory = categoryLine + " " + subcategory
+                        if (categorySubcategory not in categorySubcatDB):
+                            categorySubcatDB.append(categorySubcategory)
+                            insert("categorysubcategory", "category_type, subcategory_type", strDB(categoryLine) + ", " + strDB(subcategory))
+
         elif (line == "END"):
-            reading = False
+            readingRecipe = False
     
-            
+    file.close()
         
 
-
-
-#cur.close()
-#conn.close()
 main()
+cur.close()
+conn.close()
